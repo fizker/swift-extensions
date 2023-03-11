d8111d5cad533fc246a93956f93fbc90781de590
Benjamin Horsleben <benjamin@fizkerinc.dk>
2023-03-11 17:26:01 +0100

    Added encoder and decoder strategies for using JSONEncoder and JSONDecoder

--- Sources/FzkExtensions/Date+ISO8601WithFractionalSeconds.swift
+++ Sources/FzkExtensions/Date+ISO8601WithFractionalSeconds.swift
@@ -1,19 +1,40 @@
 import Foundation
 
 extension Formatter {
-	static let iso8601WithFractions = ISO8601DateFormatter() ~ {
+	static let iso8601WithFractionalSeconds = ISO8601DateFormatter() ~ {
 		$0.formatOptions = [ .withFractionalSeconds, .withInternetDateTime ]
 	}
 }
 
 public extension Date {
 	init?(iso8601: String) {
-		guard let d = Formatter.iso8601WithFractions.date(from: iso8601)
+		guard let d = Formatter.iso8601WithFractionalSeconds.date(from: iso8601)
 		else { return nil }
 		self = d
 	}
 
 	var iso8601: String {
-		return Formatter.iso8601WithFractions.string(from: self)
+		return Formatter.iso8601WithFractionalSeconds.string(from: self)
+	}
+}
+
+public extension JSONEncoder.DateEncodingStrategy {
+	static let iso8601WithFractionalSeconds = custom {
+		var container = $1.singleValueContainer()
+		try container.encode($0.iso8601)
+	}
+}
+
+public extension JSONDecoder.DateDecodingStrategy {
+	static let iso8601WithFractionalSeconds = custom {
+		let container = try $0.singleValueContainer()
+		let string = try container.decode(String.self)
+
+		guard let date = Date(iso8601: string)
+		else {
+			throw DecodingError.dataCorrupted(.init(codingPath: $0.codingPath, debugDescription: "Invalid date"))
+		}
+
+		return date
 	}
 }
--- Tests/ExtensionsTests/Date+ISO8601WithFractionalSecondsTests.swift
+++ Tests/ExtensionsTests/Date+ISO8601WithFractionalSecondsTests.swift
@@ -3,6 +3,11 @@ import FzkExtensions
 
 final class DateISO8601WithFractionalSecondsTests: XCTestCase {
 	let iso8601 = "2019-03-22T13:58:24.371Z"
+	var json: String {
+		"""
+		"\(iso8601)"
+		"""
+	}
 	let timeInterval: TimeInterval = 1_553_263_104.371
 
 	func test__dateInitWithString__valueHasFractionalSeconds__decodesCorrectly() async throws {
@@ -15,6 +20,25 @@ final class DateISO8601WithFractionalSecondsTests: XCTestCase {
 		let value = Date(timeIntervalSince1970: timeInterval)
 		let expected = iso8601
 		let actual = value.iso8601
+		XCTAssertEqual(expected, actual)
+	}
+
+	func test__jsonDecoder__valueHasFractionalSeconds__decodesCorrectly() async throws {
+		let decoder = JSONDecoder()
+		decoder.dateDecodingStrategy = .iso8601WithFractionalSeconds
+		let data = json.data(using: .utf8)!
+		let actual = try decoder.decode(Date.self, from: data)
+		let expected = Date(timeIntervalSince1970: timeInterval)
+		XCTAssertEqual(expected, actual)
+	}
+
+	func test__jsonEncoder__valueHasFractionalSeconds__encodesCorrectly() async throws {
+		let encoder = JSONEncoder()
+		encoder.dateEncodingStrategy = .iso8601WithFractionalSeconds
+		let data = try encoder.encode(Date(timeIntervalSince1970: timeInterval))
+		let actual = String(data: data, encoding: .utf8)
+		let expected = json
+
 		XCTAssertEqual(expected, actual)
 	}
 }
